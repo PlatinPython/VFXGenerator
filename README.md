1a82d3a8791ea5e101409de8eae8eb6014a111bb
PlatinPython <robert.alexander.kaempf@gmail.com>
2021-07-26 10:46:34 +0200

    Initial setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Particle Generator
+Adds a block that can generate custom particles
+
+### Currently WIP
---
+++ src/main/java/platinpython/particlegenerator/ParticleGenerator.java
@@ -0,0 +1,8 @@
+package platinpython.particlegenerator;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ParticleGenerator.MOD_ID)
+public class ParticleGenerator {
+	public static final String MOD_ID = "particlegenerator";
+}
