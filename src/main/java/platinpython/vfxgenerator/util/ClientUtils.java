a568600b337b6faef081408f1ff7f167cfd26b8a
PlatinPython <robert.alexander.kaempf@gmail.com>
2021-08-17 00:11:02 +0200

    Added real block model (somewhat WIP)

--- src/main/java/platinpython/vfxgenerator/util/ClientUtils.java
+++ src/main/java/platinpython/vfxgenerator/util/ClientUtils.java
@@ -1,6 +1,8 @@
 package platinpython.vfxgenerator.util;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.client.renderer.texture.AtlasTexture;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.vector.Vector3d;
@@ -10,13 +12,20 @@ import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import platinpython.vfxgenerator.VFXGenerator;
 import platinpython.vfxgenerator.client.gui.screen.VFXGeneratorScreen;
 import platinpython.vfxgenerator.client.particle.VFXParticle;
 import platinpython.vfxgenerator.tileentity.VFXGeneratorTileEntity;
+import platinpython.vfxgenerator.util.registries.BlockRegistry;
 
 @EventBusSubscriber(modid = VFXGenerator.MOD_ID, bus = Bus.MOD, value = Dist.CLIENT)
 public class ClientUtils {
+	@SubscribeEvent
+	public static void init(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(BlockRegistry.VFX_GENERATOR.get(), RenderType.cutout());
+	}
+
 	@SuppressWarnings("deprecation")
 	@SubscribeEvent
 	public static void onTextureStich(TextureStitchEvent.Pre event) {
@@ -31,7 +40,7 @@ public class ClientUtils {
 		VFXParticle particle = new VFXParticle(minecraft.level, minecraft.particleEngine.textureAtlas.getSprite(new ResourceLocation(VFXGenerator.MOD_ID, "particle/" + spriteName)), color, lifetime, size, pos, motion, gravity, collision);
 		minecraft.particleEngine.add(particle);
 	}
-	
+
 	public static void openVFXGeneratorScreen(VFXGeneratorTileEntity tileEntity) {
 		Minecraft.getInstance().setScreen(new VFXGeneratorScreen(tileEntity));
 	}
